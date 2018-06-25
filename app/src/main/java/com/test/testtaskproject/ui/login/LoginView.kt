a70cf542ea9262d30329945752f2390979dad219
Sergey Shvets <ser.shveps@gmail.com>
2018-06-25 11:35:53 +0300

    Created interface LoginView

---
+++ app/src/main/java/com/test/testtaskproject/ui/login/LoginView.kt
@@ -0,0 +1,15 @@
+package com.test.testtaskproject.ui.login
+
+import android.support.annotation.IdRes
+import com.test.testtaskproject.ui.base.BaseView
+
+/**
+ * Created by Sergey Shvets on 25/06/2018.
+ */
+interface LoginView : BaseView {
+
+    fun showEmailValidError(@IdRes resError: Int)
+
+    fun showPassValidError(@IdRes resError: Int)
+
+}
