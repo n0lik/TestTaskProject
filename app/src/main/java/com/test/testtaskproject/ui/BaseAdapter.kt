ba93490dba0a515025a390f9f9c9fa1643e172e6
Sergey Shvets <ser.shveps@gmail.com>
2018-06-25 11:23:29 +0300

    Created abstract BaseAdapter

---
+++ app/src/main/java/com/test/testtaskproject/ui/BaseAdapter.kt
@@ -0,0 +1,30 @@
+package com.test.testtaskproject.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by Sergey Shvets on 25/06/2018.
+ */
+abstract class BaseAdapter<T>: RecyclerView.Adapter<BaseViewHolder<T>>() {
+
+    private val list = ArrayList<T>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T> {
+        val view = LayoutInflater.from(parent.context).inflate(getLayoutId(), parent, false)
+        return createViewHolder(view)
+    }
+
+    protected abstract fun getLayoutId(): Int
+
+    protected abstract fun createViewHolder(view: View): BaseViewHolder<T>
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
+        holder.bind(list[position])
+    }
+
+}
---
+++ app/src/main/java/com/test/testtaskproject/ui/BaseViewHolder.kt
@@ -0,0 +1,13 @@
+package com.test.testtaskproject.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by Sergey Shvets on 25/06/2018.
+ */
+abstract class BaseViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    abstract fun bind(item: T)
+
+}
