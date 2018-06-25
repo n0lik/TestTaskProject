a0e104fee07db898a3a1d6a3c4e56c81dc7c6637
Sergey Shvets <ser.shveps@gmail.com>
2018-06-25 10:00:34 +0300

    Created BasePresenterImpl

---
+++ app/src/main/java/com/test/testtaskproject/ui/BasePresenterImpl.kt
@@ -0,0 +1,19 @@
+package com.test.testtaskproject.ui
+
+/**
+ * Created by Sergey Shvets on 25/06/2018.
+ */
+abstract class BasePresenterImpl<V: BaseView>: BasePresenter<V> {
+
+    private var view: V? = null
+
+    override fun bind(view: V) {
+        this.view = view
+    }
+
+    override fun unbind() {
+        this.view = null
+    }
+
+    override fun getView(): V? = view
+}
