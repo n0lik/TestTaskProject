b61631ff56c5e3c23d202c4190996dff30e65492
Sergey Shvets <ser.shveps@gmail.com>
2018-06-25 14:47:40 +0300

    Added CompositeDisposable to BasePresenterImpl

--- app/src/main/java/com/test/testtaskproject/ui/base/BasePresenterImpl.kt
+++ app/src/main/java/com/test/testtaskproject/ui/base/BasePresenterImpl.kt
@@ -1,18 +1,27 @@
 package com.test.testtaskproject.ui.base
 
+import io.reactivex.disposables.CompositeDisposable
+
 /**
  * Created by Sergey Shvets on 25/06/2018.
  */
 abstract class BasePresenterImpl<V: BaseView>: BasePresenter<V> {
 
+    protected var compositeDisposable: CompositeDisposable? = null
     private var view: V? = null
 
     override fun bind(view: V) {
         this.view = view
+        compositeDisposable = CompositeDisposable()
     }
 
     override fun unbind() {
         this.view = null
+        compositeDisposable?.apply {
+            if (!isDisposed) {
+                this.dispose()
+            }
+        }
     }
 
     override fun getView(): V? = view
