63910f3473d274542d91aead661ab19cd3bda62d
Sergey Shvets <ser.shveps@gmail.com>
2018-06-25 14:00:46 +0300

    Created WorldView interface

---
+++ app/src/main/java/com/test/testtaskproject/ui/worlds/WorldView.kt
@@ -0,0 +1,13 @@
+package com.test.testtaskproject.ui.worlds
+
+import com.test.testtaskproject.model.WorldModel
+import com.test.testtaskproject.ui.base.BaseView
+
+/**
+ * Created by Sergey Shvets on 25/06/2018.
+ */
+interface WorldView: BaseView {
+
+    fun showWorlds(list: List<WorldModel>, clear: Boolean)
+
+}
