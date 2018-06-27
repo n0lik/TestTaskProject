134771d59e7614e91bb73d2699de582278e81ba0
Sergey Shvets <ser.shveps@gmail.com>
2018-06-27 09:46:05 +0300

    Added field "deviceType" to User model

--- app/src/main/java/com/test/testtaskproject/model/user/User.kt
+++ app/src/main/java/com/test/testtaskproject/model/user/User.kt
@@ -3,4 +3,4 @@ package com.test.testtaskproject.model.user
 /**
  * Created by Sergey Shvets on 25/06/2018.
  */
-data class User (val email: String, val pass: String)
+data class User (val email: String, val pass: String, val deviceType: String)
